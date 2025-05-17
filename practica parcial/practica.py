acf6bc188b04f0416d38e06348746df500bb42d2
Josue Trujillo <josuetrujillo83@gmail.com>
2025-05-17 09:17:13 -0300

    Actualizacion de la resolucion usando ciclos for

--- practica parcial/practica.py
+++ practica parcial/practica.py
@@ -7,15 +7,34 @@ mapa = [
 
 
 def verificar_tesoro(mapa: list, x: int, y: int) -> int:
-    x_tesoro = 1
+
+    # Solucion al entregralo
+    """ x_tesoro = 1
     y_tesoro = 3
 
-    distancia = x - x_tesoro + y - y_tesoro
+    distancia = x - x_tesoro + y - y_tesoro """
 
+    # Solucion completa (Actualizado despues de entregarlo)
+    for i in range(len(mapa)):
+        for j in range(len(mapa[i])):
+            if mapa[i][j] == 1:
+                x_tesoro = i
+                y_tesoro = j
 
-    # Convertir la distancia en un numero positivo
-    if distancia < 0:
-        distancia *= (-1)
+                calculo_x = x - x_tesoro
+                calculo_y = y - y_tesoro
+
+                if calculo_x < 0:
+                    calculo_x *= -1
+                elif calculo_y < 0:
+                    calculo_y *= -1
+
+                distancia = calculo_x + calculo_y
+    #------------------------------------------------------
+
+    # Convertir la distancia a un numero positivo
+    """ if distancia < 0:
+        distancia *= (-1) """
 
     return distancia
 
@@ -37,7 +56,7 @@ while opcion == "s":
         print("¡Encontraste el tesoro!")
         opcion = "n"
     else:
-        
+
         print(f"Fallaste. El tesoro está a {resultado} casilleros de distancia.")
         opcion = input("Queres seguir intentando? (s/n): ")
 
