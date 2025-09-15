ca01ede2c61255eec6c04488a94a7d15a8ce5146
Josue Trujillo <josuetrujillo83@gmail.com>
2025-09-15 20:41:56 -0300

    fix

--- Videojuego_consola/datos.py
+++ Videojuego_consola/datos.py
@@ -1,3 +1,10 @@
-posicion = 15
-tablero = [0,1,0,0,0,3,0,0,0,0,0,1,0,0,2,1,1,0,0,0,1,0,0,2,0,0,0,1,0,0,0]
-posicion_ganador = len(tablero) - 1
+tablero = [0,1,0,0,0,3,0,0,1,0,0,1,0,1,2,1,1,0,0,0,1,0,0,2,0,0,0,1,0,0,0]
+posicion = { "valor": 15}
+posicion_ganadora = len(tablero)-1
+
+tamaño_pantalla = (1600,900)
+tamaño_jugador = (50, 80)
+volumen_por_defecto = 0.1
+
+tamaño_tempo = (50, 50)
+tamaño_moneda = (50, 40)
